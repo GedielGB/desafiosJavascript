45f8eb2179d3d469e68ee3bb97be2fa8fe75e5cc
GedielGB <83519908+GedielGB@users.noreply.github.com>
2022-04-12 21:59:51 -0300

    Add files via upload

---
+++ comparacaoOperador/comparacaoOperador.js
@@ -0,0 +1,7 @@
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+
+function myFunction(a, b) {
+   
+    return a === b;
+ }
